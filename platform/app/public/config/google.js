01c73abb0f7846bded7b66d50584bea9b3739ab2
zryann <zryan.muhammed.jaafer@gmail.com>
2025-01-28 01:05:54 +0300

    feat(config): update Google Cloud configuration for authentication

--- platform/app/public/config/google.js
+++ platform/app/public/config/google.js
@@ -1,24 +1,18 @@
 /** @type {AppTypes.Config} */
 window.config = {
   routerBasename: '/',
-  enableGoogleCloudAdapter: false,
-  // below flag is for performance reasons, but it might not work for all servers
+  enableGoogleCloudAdapter: true, // Must be true for Google Cloud auth
   showWarningMessageForCrossOrigin: true,
   showCPUFallbackMessage: true,
   showLoadingIndicator: true,
   strictZSpacingForVolumeViewport: true,
-  // This is an array, but we'll only use the first entry for now
   oidc: [
     {
-      // ~ REQUIRED
-      // Authorization Server URL
       authority: 'https://accounts.google.com',
-      client_id: '723928408739-k9k9r3i44j32rhu69vlnibipmmk9i57p.apps.googleusercontent.com',
+      client_id: '61487220449-ic5f6o4l2o6gdjt6mvc5g909ehupagi6.apps.googleusercontent.com',
       redirect_uri: '/callback',
       response_type: 'id_token token',
-      scope:
-        'email profile openid https://www.googleapis.com/auth/cloudplatformprojects.readonly https://www.googleapis.com/auth/cloud-healthcare', // email profile openid
-      // ~ OPTIONAL
+      scope: 'email profile openid https://www.googleapis.com/auth/cloud-healthcare',
       post_logout_redirect_uri: '/logout-redirect.html',
       revoke_uri: 'https://accounts.google.com/o/oauth2/revoke?token=',
       automaticSilentRenew: true,
@@ -28,21 +22,20 @@ window.config = {
   extensions: [],
   modes: [],
   showStudyList: true,
-  // filterQueryParam: false,
   defaultDataSourceName: 'dicomweb',
   dataSources: [
     {
       namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
       sourceName: 'dicomweb',
       configuration: {
-        friendlyName: 'dcmjs DICOMWeb Server',
+        friendlyName: 'Clinic DICOM Server',
         name: 'GCP',
         wadoUriRoot:
-          'https://healthcare.googleapis.com/v1/projects/ohif-cloud-healthcare/locations/us-east4/datasets/ohif-qa-dataset/dicomStores/ohif-qa-2/dicomWeb',
+          'https://healthcare.googleapis.com/v1/projects/clinic-449120/locations/us-central1/datasets/clinic-dataset/dicomStores/clinic-dicom-store/dicomWeb',
         qidoRoot:
-          'https://healthcare.googleapis.com/v1/projects/ohif-cloud-healthcare/locations/us-east4/datasets/ohif-qa-dataset/dicomStores/ohif-qa-2/dicomWeb',
+          'https://healthcare.googleapis.com/v1/projects/clinic-449120/locations/us-central1/datasets/clinic-dataset/dicomStores/clinic-dicom-store/dicomWeb',
         wadoRoot:
-          'https://healthcare.googleapis.com/v1/projects/ohif-cloud-healthcare/locations/us-east4/datasets/ohif-qa-dataset/dicomStores/ohif-qa-2/dicomWeb',
+          'https://healthcare.googleapis.com/v1/projects/clinic-449120/locations/us-central1/datasets/clinic-dataset/dicomStores/clinic-dicom-store/dicomWeb',
         qidoSupportsIncludeField: true,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
